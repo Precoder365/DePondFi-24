dbb25e924dd77412e5f5e7fd249028692decb258
precoder365 <prenitharajesh04@gmail.com>
2024-06-15 09:55:45 +0530

    update training
    Signed-off-by: precoder365 <prenitharajesh04@gmail.com>

--- train_fishy.py
+++ train_fishy.py
@@ -1,38 +1,22 @@
+import yaml
 from ultralytics import YOLO
 
+data_yaml = dict(
+    train ='train',
+    val ='valid',
+    test='test',
+    nc =9,
+    names =['Mouth','Eye','Top fin','Fish center','Bottom center','start point tail','top outline tail','mid outline tail','bottom outline tail']
+)
 
-model = YOLO('yolov8n-pose.pt') 
+with open('data.yaml', 'w') as outfile:
+    yaml.dump(data_yaml, outfile, default_flow_style=True)
+
+names =['Mouth','Eye','Top fin','Fish center','Bottom center','start point tail','top outline tail','mid outline tail','bottom outline tail']
+M=list(range(len(names)))
+class_map=dict(zip(M,names))
+
+model = YOLO("yolov8x.pt")
 
 model.train(data='config.yaml', epochs=1, imgsz=640)
 
-# import os
-# import yaml
-# from ultralytics import YOLO
-
-# # Function to create data.yaml
-# def create_data_yaml(data_dir):
-#     data_yaml = {
-#         'path': data_dir,
-#         'train': 'images/train',
-#         'val': 'images/val',
-#         'nc': 9,
-#         'names': ['mouth', 'eye', 'top_fin', 'fish_center', 'bottom_center', 'start_point_tail', 'top_outline_tail', 'mid_outline_tail', 'bottom_outline_tail'],
-#         'kpt_shape': [9,2]
-#     }
-    
-#     with open('data.yaml', 'w') as outfile:
-#         yaml.dump(data_yaml, outfile, default_flow_style=False)
-
-# # Main function
-# if __name__ == '__main__':
-#     base_dir = r'C:\DEVELOPMENT\DePondFi-24'
-#     data_dir = os.path.join(base_dir, 'data')
-    
-#     # Create data.yaml
-#     create_data_yaml(data_dir)
-    
-#     # Load YOLOv8 model
-#     model = YOLO("yolov8n-pose.pt")
-    
-#     # Train the model
-#     model.train(data='data.yaml', epochs=1, imgsz=640)
